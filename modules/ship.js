c8e3a64b62d199326d5e1d71c1af2898eb17e505
Henry Brooks <henbrooks6@gmail.com>
2023-06-22 18:34:50 +0100

    created simple ship object

---
+++ modules/ship.js
@@ -0,0 +1,24 @@
+class Ship {
+
+    constructor(length){
+        this.numHits = 0;
+        this.length = length;
+    }
+
+    //increments the hits on a ship
+    hit(){
+        numHits++;
+    }
+
+    //returns true if ship is sunk
+    isSunk(){
+        if (this.numHits == this.length){
+            return true;
+        } 
+        return false;
+    }
+
+}
+
+module.exports = Ship;
+
---
+++ modules/tests/ship.test.js
@@ -0,0 +1,8 @@
+const Ship = require("../ship");
+
+test("Constructing ship",()=>{
+    const ship = new Ship(4);
+    expect(ship.isSunk).toBe(false);
+    expect(ship.length).toBe(4);
+    expect(ship.numHits).toBe(0);
+})
