6e96e35d0139e6cc2a8050bf03901096436cd105
Henry Brooks <henbrooks6@gmail.com>
2023-06-22 19:39:47 +0100

    method for placing ships

--- modules/gameboard.js
+++ modules/gameboard.js
@@ -1,12 +1,33 @@
 class Gameboard {
-  constructor(player) {
-    this.player = player;
-    this.ships = [];
-    this.board = new Array(64);
+  constructor(ships) {
+    this.ships = ships;
+    this.board = new Array(100);
+    this.board.fill(-1);
   }
 
   getCellIndex(hor, ver) {
     return (ver - 1) * 10 + (hor - 1);
+  }
+
+  placeShip(shipIndex, hor, ver) {
+    //puts the index of ship in the board array
+    const ship = this.ships[shipIndex];
+    if (ship.length <= ver && hor <= 10 && ver <= 10) {
+
+      //check if a ship is already placed in this location
+      for (let i = 0; i < ship.length; i++) {
+        if (this.board[this.getCellIndex(hor, ver - i)] !== -1) {
+          return false;
+        }
+      }
+      //if no ship then place ship
+      for (let i = 0; i < ship.length; i++) {
+        this.board[this.getCellIndex(hor, ver - i)] = shipIndex;
+      }
+      return true;
+    }
+    //if the ship will not fit on the board
+    return false;
   }
 }
 
--- modules/tests/gameboard.test.js
+++ modules/tests/gameboard.test.js
@@ -3,8 +3,8 @@ const Ship = require("../ship");
 
 test("grid coord to array conversion", () => {
   const gameboard = new Gameboard();
-  expect(gameboard.getCellIndex(1,1)).toBe(0)
-  expect(gameboard.getCellIndex(10,10)).toBe(99);
+  expect(gameboard.getCellIndex(1, 1)).toBe(0);
+  expect(gameboard.getCellIndex(10, 10)).toBe(99);
 });
 
 //10 x 10 grid
@@ -13,11 +13,36 @@ describe("Placing ships in correct locations", () => {
   const ship1 = new Ship(2);
   const ship2 = new Ship(3);
   const ship3 = new Ship(4);
-  const gameboard = new Gameboard();
+  const ships = [ship1, ship2, ship3];
 
   //assuming the position is determined by the top most cell of the ship
   test("vertical ship cannot be placed on bottom row", () => {
-    expect(gameboard.placeShip(ship1, [0, 0])).toBe(false);
-    expect(gameboard.placeShip(ship1, [0, 1])).toBe(true);
+    const gameboard = new Gameboard(ships);
+    expect(gameboard.placeShip(0, 1, 1)).toBe(false);
+    //expect(gameboard.placeShip(2, 1, 3)).toBe(false);
+    //expect(gameboard.placeShip(2, 3, 4)).toBe(true);
+
+    expect(gameboard.placeShip(0, 1, 2)).toBe(true);
+    expect(gameboard.board[gameboard.getCellIndex(1, 1)]).toBe(0);
+    expect(gameboard.board[gameboard.getCellIndex(1, 2)]).toBe(0);
+    expect(gameboard.board[gameboard.getCellIndex(2, 2)]).toBe(-1);
+    expect(gameboard.board[gameboard.getCellIndex(1, 3)]).toBe(-1);
   });
+
+  test("placing ship over each other", () => {
+    const gameboard = new Gameboard(ships);
+
+    expect(gameboard.placeShip(0, 1, 2)).toBe(true);
+    expect(gameboard.placeShip(1, 1, 4)).toBe(false);
+    expect(gameboard.placeShip(1, 1, 5)).toBe(true);
+    expect(gameboard.board[gameboard.getCellIndex(1,3)]).toBe(1);
+    expect(gameboard.board[gameboard.getCellIndex(1,2)]).toBe(0);
+  });
+
+  test("placing ship out of bounds",()=>{
+    const gameboard = new Gameboard(ships);
+    expect(gameboard.placeShip(0, 10, 11)).toBe(false);
+    expect(gameboard.placeShip(0, 11, 10)).toBe(false);
+    expect(gameboard.placeShip(0, 10, 10)).toBe(true);
+  })
 });
